f193f9f432e4feba33b95bdfbaa0be496f7bb3c0
deepak-trenzasoft <119485214+deepak-trenzasoft@users.noreply.github.com>
2023-01-02 11:38:15 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,18 +6,9 @@ import Head from "next/head";
 
 import React, { useState, useEffect } from 'react';
 
-// //import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.css';
-// //import 'bootstrap/dist/js/bootstrap.js';
-// import $ from 'jquery';
-// import Popper from 'popper.js';
-
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(()=>{
-      import("bootstrap/dist/js/bootstrap");
-  },[])
 
   return (
     <> 
